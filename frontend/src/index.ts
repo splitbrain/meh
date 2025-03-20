e392c11c58beab734d52464712fc90c61b269407
Andreas Gohr <andi@splitbrain.org>
2025-03-20 11:28:38 +0100

    more frontend framework cleanup
    removed all the unused stuff

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -8,22 +8,5 @@
  * to consume components of this package as outlined in the `README.md`.
  */
 
-// Define a type for the translations that can be used by consumers
-export type MehFormTranslations = {
-  formTitle?: string;
-  nameLabel?: string;
-  namePlaceholder?: string;
-  emailLabel?: string;
-  emailPlaceholder?: string;
-  websiteLabel?: string;
-  websitePlaceholder?: string;
-  commentLabel?: string;
-  commentPlaceholder?: string;
-  submitButton?: string;
-  submittingButton?: string;
-  successMessage?: string;
-  errorPrefix?: string;
-};
-
 // Export the component types for users to implement
 export type * from './components.d.ts';
